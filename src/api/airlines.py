3a2125e4aaec878e411b5eb23b74113b85da2dd4
Izzat Z <izzat.zainir11@gmail.com>
2022-10-15 23:05:16 +0800

    Reintroduce simplejson as dependency

--- src/api/airlines.py
+++ src/api/airlines.py
@@ -1,4 +1,4 @@
-import json
+import simplejson as json
 import mariadb
 from flask import Blueprint
 
--- src/api/flights.py
+++ src/api/flights.py
@@ -1,4 +1,4 @@
-import json
+import simplejson as json
 import datetime
 import mariadb
 from flask import request
